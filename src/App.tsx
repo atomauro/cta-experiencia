0ea167af09874359f096356f5dd378fa9f4f283d
Mauricio Henao <atomauro@cosmos-2.local>
2023-08-31 09:46:09 -0500

    .

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,35 @@ import { OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import "./App.css";
 import "./folio/src/index.js";
+import headerImage from './LOGOCTA1.png'; // Ruta a tu imagen de encabezado
+
 
 function ThreeScene() {
   return (
+    <>
+    <Header/>
     <Canvas>
       <ambientLight />
       <pointLight position={[5, 5, 5]} intensity={1} />
       <pointLight position={[-3, -3, 2]} />
       <OrbitControls />
     </Canvas>
+    </>
   );
+}
+
+function Header(){
+  return(
+    <div className="header">
+    <div className="header-text">
+      <h1>DIRECTORIO</h1>
+    </div>
+    <img src={headerImage} alt="Header" className="header-image" />
+    <div className="header-buttons">
+      <button className="round-button">CREEMOS JUNTOS</button>
+    </div>
+  </div>
+  )
 }
 
 function App() {
